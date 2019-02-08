67bcdf52d74d747829d95ce443e01dda84ff90a4
liuxieric123 <liuxieric123@sina.com>
2019-02-08 11:40:40 +0800

    单链表增加哨兵节点（头结点）

--- alg/src/main/java/linked/singlelist.java
+++ alg/src/main/java/linked/singlelist.java
@@ -2,26 +2,31 @@ package linked;
 
 public class singlelist {
 
-    private SNode head = null;
+    private SNode head;
+    
+    public singlelist () {
+        head = new SNode();
+    }
 
     public void insertHead (int t) {
         SNode node = new SNode(t);
-        if (head == null) {
-            head = node;
-        } else {
-            node.next = head;
-            head.next = node;
-        }
+        head.next = node;
     }
 
     public void insertTail (int h) {
         SNode node = new SNode(h);
         SNode q = head;
-        while (q != null && q.next != null) {
+        while (q.next != null) {
             q = q.next;
         }
         q.next = node;
     }
+
+    public void deleteByIndex (int index) {
+
+    }
+
+
 
     public static class SNode{
 
@@ -36,6 +41,10 @@ public class singlelist {
 
         public SNode(int element) {
             this.element = element;
+            this.next = null;
+        }
+
+        public SNode () {
             this.next = null;
         }
 
