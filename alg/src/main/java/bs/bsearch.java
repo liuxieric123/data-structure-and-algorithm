5a97c915d54efbcc093f75ebf9e7cb41721d07a2
liuxieric123 <liuxieric123@sina.com>
2019-03-17 15:32:53 +0800

    二分查找第一个大于等于给定值

--- alg/src/main/java/bs/bsearch.java
+++ alg/src/main/java/bs/bsearch.java
@@ -59,4 +59,18 @@ public class bsearch {
         }
         return -1;
     }
+
+    public int bsFristLarge(int [] nums, int n, int val) {
+        int low = 0, high = n - 1;
+        while(low <= high) {
+            int mid = low + ((high - low) << 1);
+            if (nums[mid] < val) {
+                low = mid + 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] < val) return mid;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
