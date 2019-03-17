054a30161f5ce81a7398d6c7fc4fc2f98f858801
liuxieric123 <liuxieric123@sina.com>
2019-03-17 15:36:15 +0800

    二分查找最后一个小于等于给定值的位置

--- alg/src/main/java/bs/bsearch.java
+++ alg/src/main/java/bs/bsearch.java
@@ -73,4 +73,18 @@ public class bsearch {
         }
         return -1;
     }
+
+    public int bsLastLittle(int [] nums, int n, int val) {
+        int low = 0, high = n - 1;
+        while(low <= high) {
+            int mid = low + ((high - low) << 1);
+            if (nums[mid] > val) {
+                high = mid - 1;
+            } else {
+                if (mid == n - 1 || nums[mid+1] > val) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
