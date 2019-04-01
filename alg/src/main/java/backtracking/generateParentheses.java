78da3e07f8d54726d82b221da4a3903b98d544ac
liuxieric123 <liuxieric123@sina.com>
2019-04-01 12:32:33 +0800

    注释

--- alg/src/main/java/backtracking/generateParentheses.java
+++ alg/src/main/java/backtracking/generateParentheses.java
@@ -8,7 +8,7 @@ public class generateParentheses {
         generate(result, "", n, n);
         return result;
     }
-
+// 关键点：一左括号在什么时候放置都是合法的；二是存在的左括号数量大于右括号数量时放置右括号是合法的
     public void generate (List <String> result, String str, int left, int right) {
         if (left == 0 && right == 0) {
             result.add(str);
