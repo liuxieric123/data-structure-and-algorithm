c5af0356379bce1a1b7fb63f018ef7115430c245
liuxieric123 <liuxieric123@sina.com>
2019-12-01 19:05:39 +0800

    search insert position c++

---
+++ c++/searchInsertPosition.cc
@@ -0,0 +1,25 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public:
+    int searchInsert(vector<int>& nums, int target) {
+        if (nums.size() == 0) return 0;
+        int len = nums.size();
+        int left = 0;
+        int right = len - 1;
+        int mid = len / 2;
+        while(left <= right) {
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+            mid = left + (right - left) / 2;
+        }
+        return left;
+    }
+};
