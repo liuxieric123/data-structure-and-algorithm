40d0c684ec8c79d4147713f2ea1cbde350716417
liuxieric123 <liuxieric123@sina.com>
2019-12-04 18:58:06 +0800

    container with most water c++

---
+++ c++/containerWithMostWater.cc
@@ -0,0 +1,17 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       int area = 0;
+       int i = 0, j = height.size() - 1;
+       while (i < j) {
+           area = max(area, min(height[i], height[j])*(j-i));
+           if (height[i] < height[j]) ++i;
+           else --j;
+       }
+       return area;
+    }
+};
