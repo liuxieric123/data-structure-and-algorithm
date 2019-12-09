d2e91a075c360f4a9fdb91d748ef0ca04a1dda5a
liuxieric123 <liuxieric123@sina.com>
2019-12-09 22:03:44 +0800

    kth largest element in a stream c++

---
+++ c++/kthLargestElementInAStream.cc
@@ -0,0 +1,23 @@
+#include <queue>
+
+using std::priority_queue;
+
+class KthLargest {
+public:
+
+    KthLargest(int k, vector<int>& nums): n(k) {
+        for (auto i : nums) {
+            pq.push(i);
+            if (pq.size() > n) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > n) pq.pop();
+        return pq.top();
+    }
+private:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int n;
+};
