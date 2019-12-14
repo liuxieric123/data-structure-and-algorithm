cac23627d9bdd30288d607c835de058307a428ef
liuxieric123 <liuxieric123@sina.com>
2019-12-14 17:14:31 +0800

    coin change code optimize

--- c++/coinChange.cc
+++ c++/coinChange.cc
@@ -22,4 +22,26 @@ public:
         }
         return states[size-1][amount] == INT_MAX ? -1 : states[size-1][amount];
     }
+
+    int coinChange1(vector<int>& coins, int amount) {
+        int size = coins.size();
+        int states[amount+1];
+        for (int i = 0; i <= amount; ++i) {
+            if (i % coins[0] == 0) states[i] = i / coins[0];
+            else states[i] = INT_MAX;
+        }
+        for (int i = 1; i < size; ++i) {
+            for (int j = coins[i]; j <=amount; ++j) {
+                int up = states[j];
+                int left;
+                if (states[j-coins[i]] != INT_MAX) {
+                    left = states[j-coins[i]] + 1;
+                } else {
+                    left = INT_MAX;
+                }
+                states[j] = min(up, left);
+            }
+        }
+        return states[amount] == INT_MAX ? -1 : states[amount];
+    }
 };
