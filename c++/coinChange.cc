eca56c9d78914e45598da00960774514ef0a2939
liuxieric123 <liuxieric123@sina.com>
2019-12-14 17:07:38 +0800

    coin change c++

---
+++ c++/coinChange.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int size = coins.size();
+        int states[size][amount+1];
+        for (int i = 0; i <= amount; ++i) {
+            if (i % coins[0] == 0) states[0][i] = i / coins[0];
+            else states[0][i] = INT_MAX;
+        }
+        for (int i = 1; i < size; ++i) {
+            states[i][0] = 0;
+            for (int j = 1; j <= amount; ++j) {
+                int up = states[i-1][j];
+                int left;
+                if (j-coins[i] >= 0 && states[i][j-coins[i]] != INT_MAX) {
+                    left = states[i][j-coins[i]] + 1;
+                } else {
+                    left = INT_MAX;
+                }
+                states[i][j] = min(up, left);
+            }
+        }
+        return states[size-1][amount] == INT_MAX ? -1 : states[size-1][amount];
+    }
+};
