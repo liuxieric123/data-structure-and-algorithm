73a0c1de94430ecaee3a75e8ebb90f426dba73d6
liuxieric123 <liuxieric123@sina.com>
2019-12-15 13:25:04 +0800

    intersection of two array II tow pointer

--- c++/intersectionOfTwoArrayII.cc
+++ c++/intersectionOfTwoArrayII.cc
@@ -17,4 +17,26 @@ public:
         }
         return ret;
     }
+
+     vector<int> intersect1(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> ret;
+        int pointer_1 = 0;
+        int pointer_2 = 0;
+        int size_1 = nums1.size();
+        int size_2 = nums2.size();
+        while(pointer_1 < size_1 && pointer_2 < size_2) {
+            if (nums1[pointer_1] == nums2[pointer_2]) {
+                ret.push_back(nums1[pointer_1]);
+                ++pointer_1;
+                ++pointer_2;
+            } else if (nums1[pointer_1] < nums2[pointer_2]) {
+                ++pointer_1;
+            } else {
+                ++pointer_2;
+            }
+        }
+        return ret;
+    }
 };
