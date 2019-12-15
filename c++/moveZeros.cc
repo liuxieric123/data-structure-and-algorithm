c5f42f6b0be712a74b7c6dc669be5cd79c225f2d
liuxieric123 <liuxieric123@sina.com>
2019-12-15 16:47:09 +0800

    move zeros c++

---
+++ c++/moveZeros.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int size = nums.size();
+        int f = 0;
+        int s = 0;
+        while(f != size) {
+            if (nums[f] != 0) {
+                nums[s] = nums[f];
+                ++f;
+                ++s;
+            } else {
+                ++f;
+            }
+        }
+        for (int i = s; i < size; ++i) {
+            nums[i] = 0;
+        }
+        return;
+    }
+};
