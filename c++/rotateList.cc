a9a9aa385f83fd788fc39f8de0668b9f98cb5fba
liuxieric123 <liuxieric123@sina.com>
2019-12-16 09:31:49 +0800

    rotate list two pointer c++

--- c++/rotateList.cc
+++ c++/rotateList.cc
@@ -22,4 +22,26 @@ public:
         head -> next = NULL;
         return handle;
     }
+
+    ListNode* rotateRight1(ListNode* head, int k) {
+        if (head == NULL || k <=0) return head;
+        int count = 1;
+        ListNode * handle = head;
+        while(handle -> next != NULL) {
+            ++count;
+            handle = handle -> next;
+        }
+        k %= count;
+        ListNode * pre = head;
+        ListNode * after = head;
+        for (int i = 0; i < k; ++i) pre = pre -> next;
+        while(pre != NULL && pre -> next != NULL) {
+            pre = pre -> next;
+            after = after -> next;
+        }
+        pre -> next = head;
+        head = after -> next;
+        after -> next = NULL;
+        return head;
+    }
 };
