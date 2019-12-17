3455edee52eeddf3e4c328d175c8017c96131ce5
liuxieric123 <liuxieric123@sina.com>
2019-12-17 12:53:46 +0800

    longest increase subsequence binary search c++

--- c++/longestIncreasingSubsequence.cc
+++ c++/longestIncreasingSubsequence.cc
@@ -16,4 +16,33 @@ public:
         }
         return len;
     }
+
+    int lengthOfLIS1(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        vector<int> states;
+        states.push_back(INT_MAX);
+        int len = 1;
+        for (auto i : nums) {
+            int index = binarySearch(states, len, i);
+            states[index] = i;
+            if ((index+1) == len) {
+                states.push_back(INT_MAX);
+                ++len;
+            }
+        }
+        return states.size() - 1;
+    }
+private:
+    // 找到的一定是大于等于目标值的
+    int binarySearch(vector<int> & nums, int len, int target) {
+        int low = 0, high = len - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+    
 };
