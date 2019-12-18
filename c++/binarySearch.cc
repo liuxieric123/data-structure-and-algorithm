344556bdc683df70f9a51c7bbc5a07c4e6d1fede
liuxieric123 <liuxieric123@sina.com>
2019-12-18 09:28:18 +0800

    binary search with lower_bound

--- c++/binarySearch.cc
+++ c++/binarySearch.cc
@@ -21,4 +21,15 @@ class Solution {
         }
         return -1;
     }
+
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) low = mid + 1;
+            else high = mid;
+        }
+        if (nums[low] == target) return low;
+        else return -1;
+    }
 };
