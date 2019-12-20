9c96733709df3a1376549835cd44741fa866ec75
liuxieric123 <liuxieric123@sina.com>
2019-12-20 14:50:26 +0800

    clone graph bfs

--- c++/cloneGraph.cc
+++ c++/cloneGraph.cc
@@ -24,6 +24,40 @@ public:
         }
         return copy;
     }
+
+     Node* cloneGraph1(Node* node) {
+        queue<Node*> qu_node;
+        unordered_map<Node*, Node*> map_node;
+        unordered_set<Node*> set_node;
+        qu_node.push(node);
+        while(!qu_node.empty()) {
+            Node *cur = qu_node.front(); qu_node.pop();
+            if (set_node.find(cur) != set_node.end()) continue;
+            Node *copy;
+            if(map_node.find(cur) == map_node.end()) {
+                vector<Node*> tmp;
+                copy = new Node(cur -> val, tmp);
+                map_node.insert({cur, copy});
+            } else {
+                copy = map_node[cur];
+            }
+            for (auto p : cur -> neighbors) {
+                Node *neighbor;
+                if (map_node.find(p) == map_node.end()) {
+                    vector<Node*> tmp;
+                    neighbor = new Node(p -> val, tmp);
+                    map_node.insert({p, neighbor});
+                }
+                else {
+                    neighbor = map_node[p];
+                }
+                copy -> neighbors.push_back(neighbor);
+                qu_node.push(p);
+            }
+            set_node.insert(cur);
+        }
+        return map_node[node];
+    }
 private:
     unordered_map<Node*, Node*> map_node;
 };
