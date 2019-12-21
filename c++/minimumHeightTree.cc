88296e0b4852ac971fdfad3ed6e58ec3e09e17ef
liuxieric123 <liuxieric123@sina.com>
2019-12-21 20:06:23 +0800

    minimum height trees c++

--- c++/minimumHeightTree.cc
+++ c++/minimumHeightTree.cc
@@ -1,4 +1,42 @@
-vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1) return vector<int>{0};
+        map<int, vector<int>> adj;
+        for (auto v: edges) {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        int Degree[n];
+        queue<int> leaves;
+        for (int i = 0; i < n; ++i) {
+            Degree[i] = adj[i].size();
+            if (Degree[i] == 1) leaves.push(i);
+        }
+        while(leaves.size() < n) {
+            int size = leaves.size();
+            n -= size;
+            for (int i = 0; i < size; ++i) {
+                int vertex = leaves.front();
+                leaves.pop();
+                for (auto v : adj[vertex]) {
+                    if (--Degree[v] == 1) leaves.push(v);
+                }
+                
+            }
+        }
+        vector<int> ret;
+        int size = leaves.size();
+        for (int i = 0; i < size; ++i) {
+            ret.push_back(leaves.front());
+            leaves.pop();
+        }
+        return ret;
+
+    }
+
+    vector<int> findMinHeightTrees1(int n, vector<vector<int>>& edges) {
         map<int, vector<int>> adj;
         for (auto v: edges) {
             adj[v[0]].push_back(v[1]);
@@ -32,3 +70,6 @@ vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         visited[start] = false;
         return maxHeight + 1;
     }
+
+
+};
