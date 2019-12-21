b49e715cc9cc2e3e327e4ff4e55f41ce12ca5eaa
liuxieric123 <liuxieric123@sina.com>
2019-12-21 14:55:00 +0800

    course schedule with DFS

--- c++/courseSchedule.cc
+++ c++/courseSchedule.cc
@@ -31,4 +31,34 @@ public:
         }
         return count == numCourses;
     }
+
+    bool canFinish1(int numCourses, vector<vector<int>>& prerequisites) {
+        map<int, vector<int>> adj;
+        for (auto v: prerequisites) {
+            adj[v[1]].push_back(v[0]);
+        }
+        bool visited[numCourses];
+        bool checked[numCourses];
+        for (int i = 0; i< numCourses; ++i) {
+            visited[i] = false;
+            checked[i] = false;
+        }
+        for (int i = 0; i < numCourses; ++i) {
+            if (!checked[i] && hasCycle(adj, i, visited, checked))
+                return false;
+        }
+        return true;
+    }
+
+    bool hasCycle(map<int, vector<int>>& adj, int start, bool *visited, bool *checked) {
+        if (visited[start]) return true;
+        visited[start] = true;
+        for (auto vertex: adj[start]) {
+            if (!checked[vertex] && hasCycle(adj, vertex, visited, checked))
+                return true;
+        }
+        checked[start] = true;
+        visited[start] = false;
+        return false;
+    }
 };
