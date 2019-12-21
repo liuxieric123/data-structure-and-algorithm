82fb6518495a1615f6e4dae19b435dcc93edd29d
liuxieric123 <liuxieric123@sina.com>
2019-12-21 20:57:34 +0800

    binary tree maximum path sum

--- c++/binaryTreeMaximumPathSum.cc
+++ c++/binaryTreeMaximumPathSum.cc
@@ -33,4 +33,18 @@ public:
         *maxSum = max(*maxSum, root->val + left + right);
         return max(root->val+left, root->val+right);
     }
+
+    int maxPathSum1(TreeNode* root) {
+        int maxSum = INT_MIN;
+        int ret = dfs(root, maxSum);
+        return ret > maxSum ? ret : maxSum;
+    }
+
+    int dfs1(TreeNode* root, int &maxSum) {
+        if (root == NULL) return 0;
+        int left = max(dfs(root -> left, maxSum), 0);
+        int right = max(dfs(root -> right, maxSum), 0);
+        maxSum = max(maxSum, root->val + left + right);
+        return root->val + max(left, right);
+    }
 };
