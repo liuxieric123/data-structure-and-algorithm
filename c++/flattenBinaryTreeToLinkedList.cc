38d491145c27b512c780c44cbcda712737f7a278
liuxieric123 <liuxieric123@sina.com>
2019-12-22 10:44:36 +0800

    flatten binary tree to linked list c++ with preorder

--- c++/flattenBinaryTreeToLinkedList.cc
+++ c++/flattenBinaryTreeToLinkedList.cc
@@ -25,4 +25,21 @@ public:
         root -> left = NULL;
         pointer = root;
     }
+
+    void flatten1(TreeNode* root) {
+        vector<TreeNode*> ret;
+        dfs(root, ret);
+        ret.push_back(NULL);
+        for (int i = 0; i < ret.size() - 1; ++i) {
+            ret[i] -> right = ret[i+1];
+            ret[i] -> left = NULL;
+        }
+    }
+
+    void dfs(TreeNode* root, vector<TreeNode*> &vec) {
+        if (root == NULL) return;
+        vec.push_back(root);
+        dfs(root->left, vec);
+        dfs(root->right, vec);
+    }
 };
