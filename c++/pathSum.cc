d7845ee4f619047ceded64ad4ee62a9088c86423
liuxieric123 <liuxieric123@sina.com>
2019-12-22 15:14:42 +0800

    path sum c++ with dfs

---
+++ c++/pathSum.cc
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        int global = 0;
+        return dfs(root, sum, global);
+    }
+
+    bool dfs(TreeNode* root, int sum, int &global) {
+        if (root == NULL) return false;
+        global += root -> val;
+        if (root -> left == NULL && root -> right == NULL){
+            if (sum == global) return true;
+        }
+        bool ret = dfs(root -> left, sum, global) || dfs(root -> right, sum, global);
+        global -= root -> val;
+        return ret;
+    }
+};
