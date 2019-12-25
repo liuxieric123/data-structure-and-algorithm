350b84cfafbea07c3dc3cbada4395d0f2dfa1d65
liuxieric123 <liuxieric123@sina.com>
2019-12-25 15:58:10 +0800

    merge intervals code optimize c++

--- c++/mergeIntervals.cc
+++ c++/mergeIntervals.cc
@@ -17,4 +17,21 @@ public:
         ret.push_back(ans);
         return ret;
     }
+
+    vector<vector<int>> merge1(vector<vector<int>>& intervals) {
+        int size = intervals.size();
+        if (size < 2) return intervals;
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ret;
+        ret.push_back(intervals[0]);
+        for (int i = 1; i < size; ++i) {
+            int n = ret.size() - 1;
+            if (intervals[i][0] > ret[n][1]) {
+                ret.push_back(intervals[i]);
+            } else if (intervals[i][1] > ret[n][1]) {
+                ret[n][1] = intervals[i][1];
+            } else continue;
+        }
+        return ret;
+    }
 };
