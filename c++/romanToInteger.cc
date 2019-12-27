bc04c30864cc82948c844f7a063ec04532290919
liuxieric123 <liuxieric123@sina.com>
2019-12-27 14:01:44 +0800

    roman to integer

--- c++/romanToInteger.cc
+++ c++/romanToInteger.cc
@@ -15,4 +15,20 @@ public:
         }
         return ret;
     }
+    int romanToInt1(string s) {
+        unordered_map<char, int> num{{'I', 1}, {'V', 5}, 
+        {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int size = s.size();
+        if (size == 0) return 0;
+        int ret = 0;
+        int neg = 0;
+        for (int i = 0; i < size-1; ++i) {
+            if (num[s[i]] < num[s[i+1]]) 
+                neg -=num[s[i]];
+            else 
+                ret += num[s[i]];
+        }
+        ret += num[s.back()];
+        return ret + neg;
+    }
 };
