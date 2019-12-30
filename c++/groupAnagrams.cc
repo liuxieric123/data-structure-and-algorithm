b72c796bd5ab14f2e77b8bd4eaf404ffa304a286
liuxieric123 <liuxieric123@sina.com>
2019-12-30 15:50:24 +0800

    group anagrams c++

--- c++/groupAnagrams.cc
+++ c++/groupAnagrams.cc
@@ -34,4 +34,24 @@ public:
         }
         return tmp;
     }
+
+    vector<vector<string>> groupAnagrams1(vector<string>& strs) {
+        int size = strs.size();
+        if (size == 0) return vector<vector<string>> {};
+        unordered_map<string, vector<string>> key_vector;
+        for (auto str: strs) {
+            string tmp = str;
+            sort(tmp.begin(), tmp.end());
+            if (key_vector.find(tmp) == key_vector.end()) {
+                key_vector[tmp] = vector<string>{str};
+            } else {
+                key_vector[tmp].push_back(str);
+            }
+        }
+        vector<vector<string>> ret;
+        for (auto pair: key_vector) {
+            ret.push_back(pair.second);
+        }
+        return ret;
+    }
 };
